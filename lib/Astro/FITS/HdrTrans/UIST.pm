aa4c780eb149ba47fb358f9c322a13b7d548bef9
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2002-12-03 21:35:50 +0000

    - remove X_BASE, Y_BASE, X_OFFSET, Y_OFFSET, X_SCALE, Y_SCALE transations
    - add SLIT_WIDTH translation
    
    svn path=/trunk/perlmods/; revision=4652

--- lib/Astro/FITS/HdrTrans/UIST.pm
+++ lib/Astro/FITS/HdrTrans/UIST.pm
@@ -330,38 +330,6 @@ sub from_UTEND {
   return %return_hash;
 }
 
-=item B<to_X_BASE>
-
-Converts the decimal hours in the FITS header C<RABASE> into
-decimal degrees for the generic header C<X_BASE>.
-
-=cut
-
-sub to_X_BASE {
-  my $FITS_headers = shift;
-  my $return;
-  if(exists($FITS_headers->{RABASE})) {
-    $return = $FITS_headers->{RABASE} * 15;
-  }
-  return $return;
-}
-
-=item B<from_X_BASE>
-
-Converts the decimal degrees in the generic header C<X_BASE>
-into decimal hours for the FITS header C<RABASE>.
-
-=cut
-
-sub from_X_BASE {
-  my $generic_headers = shift;
-  my %return_hash;
-  if(exists($generic_headers->{X_BASE})) {
-    $return_hash{'RABASE'} = $generic_headers->{X_BASE} / 15;
-  }
-  return %return_hash;
-}
-
 =item B<to_RA_BASE>
 
 Converts the decimal hours in the FITS header C<RABASE> into
@@ -438,14 +406,10 @@ Keys are generic headers, values are FITS headers.
             RA_TELESCOPE_OFFSET  => "TRAOFF",
             SLIT_ANGLE           => "SLIT_PA",
             SLIT_NAME            => "SLITNAME",
+            SLIT_WIDTH           => "SLITWID",
             STANDARD             => "STANDARD",
             TELESCOPE            => "TELESCOP",
             WAVEPLATE_ANGLE      => "WPLANGLE",
-            Y_BASE               => "RABASE",
-            X_OFFSET             => "TDECOFF",
-            Y_OFFSET             => "TRAOFF",
-            X_SCALE              => "PIXELSI",
-            Y_SCALE              => "PIXELSI",
             X_DIM                => "DCOLUMNS",
             Y_DIM                => "DROWS",
             X_LOWER_BOUND        => "RDOUT_X1",
