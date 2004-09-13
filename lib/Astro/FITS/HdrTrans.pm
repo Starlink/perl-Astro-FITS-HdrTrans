f2c092c93d99ef0322697ce2074a867d3755112d
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2004-09-13 21:56:07 +0000

    add WFCAM to list of supported instruments, add TILE_NUMBER translated header
    svn path=/trunk/perlmods/; revision=7629

--- lib/Astro/FITS/HdrTrans.pm
+++ lib/Astro/FITS/HdrTrans.pm
@@ -188,6 +188,7 @@ our @generic_headers = qw( AIRMASS_START
                            SYSTEM_VELOCITY
                            TAU
                            TELESCOPE
+			   TILE_NUMBER
                            USER_AZIMUTH_CORRECTION
                            USER_ELEVATION_CORRECTION
                            UTDATE
@@ -237,7 +238,7 @@ Values in this list can be added to using the C<push_class> method.
 
 =cut
 
-my @valid_classes = qw/ IRCAM CGS4 UIST UFTI JCMT_GSD JCMT_GSD_DB MICHELLE SCUBA UKIRTDB /;
+my @valid_classes = qw/ IRCAM CGS4 UIST UFTI JCMT_GSD JCMT_GSD_DB MICHELLE SCUBA UKIRTDB WFCAM /;
 
 =head1 PUBLIC METHODS
 
