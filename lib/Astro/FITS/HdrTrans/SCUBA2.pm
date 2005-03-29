77b9a44a4710017a84d77350d048e410834aab2d
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-03-29 03:36:40 +0000

    add hook for scuba-2
    svn path=/trunk/perlmods/; revision=8754

---
+++ lib/Astro/FITS/HdrTrans/SCUBA2.pm
@@ -0,0 +1,135 @@
+package Astro::FITS::HdrTrans::SCUBA2;
+
+=head1 NAME
+
+Astro::FITS::HdrTrans::SCUBA2 - Translate SCUBA-2 FITS headers into generic headers and back again
+
+=head1 DESCRIPTION
+
+Converts information contained in SCUBA-2 FITS headers to and from
+generic headers. See L<Astro::FITS::HdrTrans> for a list of generic
+headers.
+
+=cut
+
+use strict;
+use vars qw/ $VERSION /;
+
+$VERSION = sprintf("%d.%03d", q$Revision$ =~ /(\d+)\.(\d+)/);
+
+# This is for the one-to-one mapping defined at the end of the class.
+our %hdr;
+
+=head1 FUNCTIONS
+
+These functions provide an interface to the class, allowing the base
+class to determine if this class is the appropriate one to use for
+the given headers.
+
+=over 4
+
+=item B<valid_class>
+
+  $valid = valid_class( \%headers );
+
+This function takes one argument: a reference to a hash containing
+the untranslated headers.
+
+This method returns true (1) or false (0) depending on if the headers
+can be translated by this method.
+
+For this class, the method will return true if the B<INSTRUME> header
+exists, and its value matches the regular expression C</^scuba/i>, or
+if the C<INSTRUMENT> header exists and its value matches the regular
+expression C</^scuba-2$/i>.
+
+=back
+
+=cut
+
+sub valid_class {
+  my $headers = shift;
+
+  if( exists( $headers->{'INSTRUME'} ) &&
+      defined( $headers->{'INSTRUME'} ) &&
+      $headers->{'INSTRUME'} =~ /^scuba-2/i ) {
+    return 1;
+  } elsif( exists( $headers->{'INSTRUMENT'} ) &&
+           defined( $headers->{'INSTRUMENT'} ) &&
+           $headers->{'INSTRUMENT'} =~ /^scuba-2$/i ) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+=head1 TRANSLATION METHODS
+
+These methods provide many-to-one mappings between FITS headers and
+generic headers. An example of a method defined in this section would
+be one that converts UT date and UT hour FITS headers into one combined
+UT datetime generic header. These mappings can also use calculations,
+for example converting a zenith distance to airmass.
+
+These methods are named backwards from the C<translate_from_FITS> and
+C<translate_to_FITS> methods in that we are translating to and from
+generic headers. As an example, a method to convert to a generic airmass
+header would be named C<to_AIRMASS>.
+
+The format of these methods is C<to_HEADER> and C<from_HEADER>.
+C<to_> methods accept a hash reference as an argument and return a scalar
+value (typically a string). C<from_> methods accept a hash reference
+as an argument and return a hash.
+
+=over 4
+
+
+=back
+
+=head1 VARIABLES
+
+=over 4
+
+=item B<%hdr>
+
+Contains one-to-one mappings between FITS headers and generic headers.
+Keys are generic headers, values are FITS headers.
+
+=cut
+
+%hdr = (
+	TELESCOPE  => 'TELESCOP',
+	INSTRUMENT => 'INSTRUME',
+       );
+
+=back
+
+=head1 REVISION
+
+ $Id$
+
+=head1 AUTHOR
+
+Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2003-2005 Particle Physics and Astronomy Research Council.
+All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place,Suite 330, Boston, MA  02111-1307, USA
+
+=cut
+
+1;
