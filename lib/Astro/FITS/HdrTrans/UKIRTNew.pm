9bd10aede4f2cee241c1e29b08a0b71678c6d84d
Malcolm J. Currie <mjc@star.rl.ac.uk>
2008-03-15 03:54:10 +0000

    Use the correct comment character.
    svn path=/trunk/perlmods/; revision=15057

--- lib/Astro/FITS/HdrTrans/UKIRTNew.pm
+++ lib/Astro/FITS/HdrTrans/UKIRTNew.pm
@@ -27,43 +27,60 @@ use Carp;
 # Inherit from UKIRT
 use base qw/ Astro::FITS::HdrTrans::UKIRT /;
 
-# Use the FITS standard DATE-OBS handling
-use Astro::FITS::HdrTrans::FITS;
-
 use vars qw/ $VERSION /;
 
 $VERSION = sprintf("%d", q$Revision$ =~ /(\d+)/);
 
-# for a constant mapping, there is no FITS header, just a generic
-# header that is constant
+# For a constant mapping, there is no FITS header, just a generic
+# header that is constant.
 my %CONST_MAP = (
 
-		);
+                );
 
-# unit mapping implies that the value propogates directly
-# to the output with only a keyword name change
-
+# Unit mapping implies that the value propogates directly
+# to the output with only a keyword name change.
 my %UNIT_MAP = (
-		DEC_TELESCOPE_OFFSET => "TDECOFF",
-		DR_RECIPE            => "RECIPE",
-		GAIN                 => "GAIN",
-		RA_TELESCOPE_OFFSET  => "TRAOFF",
-                UTDATE               => "UTDATE",
-	       );
+                 DEC_TELESCOPE_OFFSET => "TDECOFF",
+                 DR_RECIPE            => "RECIPE",
+                 EXPOSURE_TIME        => "EXP_TIME",
+                 GAIN                 => "GAIN",
+                 RA_TELESCOPE_OFFSET  => "TRAOFF",
+                 UTDATE               => "UTDATE",
+               );
 
-# Create the translation methods
+# Create the translation methods.
 __PACKAGE__->_generate_lookup_methods( \%CONST_MAP, \%UNIT_MAP );
 
 =head1 COMPLEX CONVERSIONS
 
-These methods are more complicated than a simple mapping. We have to
-provide both from- and to-FITS conversions All these routines are
+These methods are more complicated than a simple mapping.  We have to
+provide both from- and to-FITS conversions.  All these routines are
 methods and the to_ routines all take a reference to a hash and return
-the translated value (a many-to-one mapping) The from_ methods take a
-reference to a generic hash and return a translated hash (sometimes
-these are many-to-many)
+the translated value (a many-to-one mapping).  The from_ methods take
+a reference to a generic hash and return a translated hash (sometimes
+these are many-to-many).
 
 =over 4
+
+=item B<to_INST_DHS>
+
+Sets the instrument data-handling-system header.
+
+=cut
+
+sub to_INST_DHS {
+   my $self = shift;
+   my $FITS_headers = shift;
+   my $return;
+
+   if ( exists( $FITS_headers->{DHSVER} ) ) {
+      $FITS_headers->{DHSVER} =~ /^(\w+)/;
+      my $dhs = uc( $1 );
+      $return = $FITS_headers->{INSTRUME} . "_$dhs";
+   }
+
+   return $return;
+}
 
 =item B<to_UTSTART>
 
@@ -90,22 +107,23 @@ YYYY-MM-DDThh:mm:ss.
 
 =cut
 
-
 sub from_UTSTART {
-  my $self = shift;
-  my $generic_headers = shift;
+   my $self = shift;
+   my $generic_headers = shift;
 
-  # use the FITS standard parser
-  my %return_hash = Astro::FITS::HdrTrans::FITS->from_UTSTART( $generic_headers);
+# Use the FITS standard parser.
+   my %return_hash = Astro::FITS::HdrTrans::FITS->from_UTSTART( $generic_headers );
 
-  if (exists $return_hash{'DATE-OBS'}) {
-    # prior to April 2005 the UKIRT FITS headers had a trailing Z
-    # Part of the ISO8601 standard but not part of the FITS standard
-    # (which always assumes UTC)
-    $return_hash{'DATE-OBS'} .= "Z"
+   if ( exists $return_hash{'DATE-OBS'} ) {
+
+# Prior to April 2005 the UKIRT FITS headers had a trailing Z.
+# This is part of the ISO8601 standard but not part of the FITS 
+# standard (which always assumes UTC), although it was in the 
+# draft FITS agreement.
+      $return_hash{'DATE-OBS'} .= "Z"
       if $generic_headers->{UTSTART}->epoch < 1112662116;
-  }
-  return %return_hash;
+   }
+   return %return_hash;
 }
 
 =item B<to_UTEND>
@@ -134,43 +152,23 @@ YYYY-MM-DDThh:mm:ss.
 
 =cut
 
-
 sub from_UTEND {
-  my $self = shift;
-  my $generic_headers = shift;
+   my $self = shift;
+   my $generic_headers = shift;
 
-  # use the FITS standard parser
-  my %return_hash = Astro::FITS::HdrTrans::FITS->from_UTEND( $generic_headers);
+# Use the FITS standard parser.
+   my %return_hash = Astro::FITS::HdrTrans::FITS->from_UTEND( $generic_headers);
 
-  if (exists $return_hash{'DATE-END'}) {
-    # prior to April 2005 the UKIRT FITS headers had a trailing Z
-    # Part of the ISO8601 standard but not part of the FITS standard
-    # (which always assumes UTC)
-    $return_hash{'DATE-END'} .= "Z"
+   if ( exists $return_hash{'DATE-END'} ) {
+
+# Prior to April 2005 the UKIRT FITS headers had a trailing Z.
+# This is part of the ISO8601 standard but not part of the FITS 
+# standard (which always assumes UTC), although it was in the 
+# draft FITS agreement.
+      $return_hash{'DATE-END'} .= "Z"
       if $generic_headers->{UTEND}->epoch < 1112662116;
-  }
-  return %return_hash;
-}
-
-=item B<to_INST_DHS>
-
-Sets the instrument data handling system header.
-
-=cut
-
-sub to_INST_DHS {
-  my $self = shift;
-  my $FITS_headers = shift;
-  my $return;
-
-  if( exists( $FITS_headers->{DHSVER} ) ) {
-    $FITS_headers->{DHSVER} =~ /^(\w+)/;
-    my $dhs = uc($1);
-    $return = $FITS_headers->{INSTRUME} . "_$dhs";
-  }
-
-  return $return;
-
+   }
+   return %return_hash;
 }
 
 =back
@@ -187,6 +185,7 @@ C<Astro::FITS::HdrTrans>, C<Astro::FITS::HdrTrans::UKIRT>
 
 Brad Cavanagh E<lt>b.cavanagh@jach.hawaii.eduE<gt>,
 Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>.
+Malcolm J. Currie E<lt>mjc@star.rl.ac.ukE<gt>
 
 =head1 COPYRIGHT
 
@@ -196,7 +195,7 @@ All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later
+Foundation; either Version 2 of the License, or (at your option) any later
 version.
 
 This program is distributed in the hope that it will be useful,but WITHOUT ANY
@@ -205,7 +204,7 @@ PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
 this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place,Suite 330, Boston, MA  02111-1307, USA
+Place, Suite 330, Boston, MA  02111-1307, USA.
 
 =cut
 
