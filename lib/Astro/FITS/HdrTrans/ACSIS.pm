152d69010385a7f9cefcb663534c114c41444d6b
Graham Bell <g.bell@eaobservatory.org>
2021-08-13 15:04:41 -1000

    Prepare version 1.64 release

--- lib/Astro/FITS/HdrTrans/ACSIS.pm
+++ lib/Astro/FITS/HdrTrans/ACSIS.pm
@@ -36,7 +36,7 @@ use constant CLIGHT => 2.99792458e5;
 
 use vars qw/ $VERSION /;
 
-$VERSION = "1.63";
+$VERSION = "1.64";
 
 # Cache UTC definition
 our $UTC = DateTime::TimeZone->new( name => 'UTC' );
